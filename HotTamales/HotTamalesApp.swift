25c9d9e961da3a8e24b4483b4887f99d477d601c
Anne Garvey <amgarvey15@gmail.com>
2023-11-06 23:37:27 +0900

    Initial Commit

---
+++ HotTamales/HotTamalesApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotTamalesApp.swift
+//  HotTamales
+//
+//  Created by anne WD on 2023/11/06.
+//
+
+import SwiftUI
+
+@main
+struct HotTamalesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
